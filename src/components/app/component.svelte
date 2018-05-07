b7786fe9cb3a3fe7b5841e7162d2cba276383310
Antony Jones <aj@desirableobjects.co.uk>
2018-05-07 12:05:56 +0100

    weird bug

--- src/components/app/component.svelte
+++ src/components/app/component.svelte
@@ -1,1 +1,9 @@
-<h1>Hello</h1>
+<p>
+  Hello { name }, the time is <span class="the-time">{ hours }:{ minutes }:{ seconds }</span>
+</p>
+
+<style>
+  .the-time {
+    font-weight: bold;
+  }
+</style>
